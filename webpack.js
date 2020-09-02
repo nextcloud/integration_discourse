3373364967b207efc51bd48548a02995baa46ba9
Julien Veyssier <eneiluj@posteo.net>
2020-09-02 13:32:50 +0200

    fix build and image loading with new webpack config
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -3,18 +3,14 @@ const path = require('path')
 const webpack = require('webpack')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
+if (webpackConfig.entry && webpackConfig.entry.main) {
+	delete webpackConfig.entry.main
+}
+
 const config = {
-    entry: {
+	entry: {
 		personalSettings: path.join(__dirname, 'src', 'personalSettings.js'),
 		dashboard: path.join(__dirname, 'src', 'dashboard.js'),
-    },
-	module: {
-		rules: [
-			{
-				test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf)$/,
-				loader: 'url-loader',
-			},
-		],
 	},
 }
 
